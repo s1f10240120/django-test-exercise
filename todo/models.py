1366d24b250348154c97b454c6301a7d8e7da41f
s1f102401203 <s1f102401203@iniad.org>
2026-06-24 11:44:04 +0900

    Taskモデルを追加

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone  
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    due_at = models.DateTimeField(null=True, blank=True)
+    posted_at = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False)
+
+    
