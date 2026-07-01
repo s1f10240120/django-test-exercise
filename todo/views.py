1a9342e05c2e97d32344131f212e9dfe367e1720
s1f102401203 <s1f102401203@iniad.org>
2026-07-01 19:13:13 +0900

    コードの修正

--- todo/views.py
+++ todo/views.py
@@ -11,9 +11,9 @@ def index(request):
         task.save()
    
     if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
+        tasks = Task.objects.order_by('-due_at')
     else:
-        tasks = Task.objects.order_by('created_at')
+        tasks = Task.objects.order_by('-posted_at')
 
     context = {
         'tasks': tasks,
